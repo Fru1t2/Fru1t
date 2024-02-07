7deb458f3dae1a4fcf5b86efcc5eb1b85b15414a
Fru1t <tjrlcjf04@naver.com>
2024-02-07 15:25:03 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,14 +2,7 @@ arr = ["c=","c-","dz=","d-","lj","nj","s=","z="]
 word = input()
 output = 0
 
-for pattern in arr:
-    count = word.count(pattern)  # 각 패턴의 출현 횟수를 센다
-    output += count
-    word = word.replace(pattern, "가")  # 해당 패턴을 모두 제거한다
+for a in arr:
+    word = word.replace(a, "0")
 
-while "가" in word:
-    word = word.replace("가","")
-
-output += len(word)
-
-print(output)
+print(len(word))
