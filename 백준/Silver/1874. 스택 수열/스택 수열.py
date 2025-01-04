da570e32487b3cb0d1562cfdb1e2ad602573e79e
Fru1t <tjrlcjf04@naver.com>
2025-01-04 23:24:29 +0900

    [Silver II] Title: 스택 수열, Time: 104 ms, Memory: 35412 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,29 @@
+import sys
+
+number = int(sys.stdin.readline())
+stack = []
+start = 1
+output = [] 
+is_possible = True
+
+for _ in range(number):
+    command = int(sys.stdin.readline())
+    if command < start:
+        if stack and stack[-1] == command:
+            stack.pop()
+            output.append("-")
+        else:
+            is_possible = False
+            break
+    else:
+        while start <= command:
+            stack.append(start)
+            output.append("+")
+            start += 1
+        stack.pop()
+        output.append("-")
+
+if is_possible:
+    print("\n".join(output))
+else:
+    print("NO")
